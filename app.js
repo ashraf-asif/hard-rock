fe966b435e51e04bd0edf3d58ea032b3b61e52a6
ashraf-asif <60070711+ashraf-asif@users.noreply.github.com>
2020-08-18 10:27:35 +0600

    hard rock done

--- app.js
+++ app.js
@@ -2,84 +2,76 @@
 const inputSongTitle = document.querySelector("#input-song-title");
 const searchSong = document.querySelector("#search-song");
 const searchResults = document.querySelector(".search-results");
+const songLyrics = document.querySelector(".song-lyrics");
 // API
 const API = "https://api.lyrics.ovh/";
 
 // click to search
-searchSong.addEventListener("click", function () {
-  searchResults.innerHTML = "";
+searchSong.addEventListener("click", (e) => {
+	e.preventDefault();
 	const suggestionFor = inputSongTitle.value;
-	fetch(`${API}suggest/${suggestionFor}`)
-		.then((res) => res.json())
-		.then((data) => {
-      const songs = data.data;
-			for (let i = 0; i < 10; i++) {
-				const song = songs[i];
-				// get song title
-				const songTitle = song.title;
-				// get artist name
-				const artistName = song.artist.name;
-				// show results
-				const songContainer = document.createElement("div");
-				songContainer.innerHTML = `<div class="songWithLyrics">
-             <div class="single-result row align-items-center my-3 p-3">
-               <div class="col-md-9">
-                 <h3 class="lyrics-name">${songTitle}</h3>
-                 <p class="author lead">Album by <span>${artistName}</span></p>
-               </div>
-               <div class="col-md-3 text-md-right text-center">
-                 <button class="get-lyrics btn btn-success" onclick="showLyrics(this)">Get Lyrics</button>
-               </div>
-             </div>
-           </div>`;
-				searchResults.appendChild(songContainer);
-			}
-		});
+
+	showResults(suggestionFor);
 });
 
-function showLyrics(e) {
-	const songContainer = e.parentElement.parentElement.parentElement;
-	// lyric's name
-	const lyricsName = songContainer.querySelector(".lyrics-name").innerText;
-	const lyricsNameInURL = encodeURIComponent(lyricsName.trim());
-	// author's name
-	const authorsName = songContainer.querySelector(".author span").innerText;
-	const authorsNameInURL = encodeURIComponent(authorsName.trim());
+const showResults = (searchValue) => {
+	fetch(`${API}suggest/${searchValue}`)
+		.then((res) => res.json())
+		.then((data) => {
+			displayData(data);
+		});
+};
+
+const displayData = (data) => {
+	const songs = data.data;
+	searchResults.innerHTML = "";
+	searchResults.style.display = "block";
+	songLyrics.style.display = "none";
+
+	songs.slice(0, 10).map((song) => {
+		const songTitle = song.title;
+		const artistName = song.artist.name;
+
+		searchResults.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                  <div class="col-md-9">
+                                    <h3 class="song-title">${songTitle}</h3>
+                                    <p class="artist-name lead">Album by <span>${artistName}</span></p>
+                                  </div>
+                                  <div class="col-md-3 text-md-right text-center">
+                                    <h1 class="get-lyrics btn btn-success" onclick="showLyrics(this)">Get Lyrics</h1>
+                                  </div>
+                               </div>`;
+	});
+};
+
+const showLyrics = (e) => {
+	const songContainer = e.parentElement.parentElement;
+	const songTitle = songContainer.querySelector(".song-title").innerText;
+	const artistName = songContainer.querySelector(".artist-name span").innerText;
 	// get lyrics
-	fetch(`${API}v1/${authorsNameInURL}/${lyricsNameInURL}`)
+	getLyrics(artistName, songTitle);
+};
+
+const getLyrics = (artist, title) => {
+	const artistInURL = encodeURIComponent(artist.trim());
+	const titleInURL = encodeURIComponent(title.trim());
+
+	fetch(`${API}v1/${artistInURL}/${titleInURL}`)
 		.then((res) => res.json())
 		.then((lyrics) => {
-			const currentSongLyric = lyrics.lyrics;
-			const lyricsContainer = document.createElement("div");
-			lyricsContainer.innerHTML = `<div class="single-lyric text-center">
-           <h2 class="text-success mb-4">${authorsName} - ${lyricsName}</h2>
-           <pre class="lyric text-white">${currentSongLyric}</pre>
+			const currentSongLyrics = lyrics.lyrics;
+			songLyrics.innerHTML = `<div class="single-lyric text-center">
+           <h2 class="close-lyrics" onclick="closeLyrics()">&#10005</h2>
+           <h2 class="text-success mb-4">${artist} - ${title}</h2>
+           <pre class="lyric text-white">${currentSongLyrics}</pre>
          </div>`;
-
-			songContainer.appendChild(lyricsContainer);
 		});
-}
 
-// get lyrics
-// fetch(`${API}v1/${artistNameInURL}/${songTitleInURL}`)
-//   .then((res) => res.json())
-//   .then((lyrics) => {
+	searchResults.style.display = "none";
+	songLyrics.style.display = "block";
+};
 
-//     const currentSongLyric = lyrics.lyrics;
-
-//   });
-
-// const getSongLyrics = (song) => {
-//   // song title
-//   const songTitle = song.title;
-//   const songTitleInURL = encodeURIComponent(songTitle.trim());
-//   // artist name
-//   const artistName = song.artist.name;
-//   const artistNameInURL = encodeURIComponent(artistName.trim());
-//   // get lyrics
-//   fetch(`${API}v1/${artistNameInURL}/${songTitleInURL}`)
-//     .then((res) => res.json())
-//     .then((lyrics) => {
-
-//     });
-// }
+const closeLyrics = () => {
+	songLyrics.innerHTML = "";
+	searchResults.style.display = "block";
+};
